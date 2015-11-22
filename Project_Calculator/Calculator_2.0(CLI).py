291aa2b3189cc02842637cce423d21cb258c6410
uitaek91 <choiuitaek@gmail.com>
2015-11-22 12:34:11 +0900

    re

--- Project_Calculator/Calculator_2.0(CLI).py
+++ Project_Calculator/Calculator_2.0(CLI).py
@@ -12,28 +12,33 @@ while True:
     value = input("Enter : ")
 
     sign = ('+', '-', '*', '/')
-    if value in sign: # If the input value isn't number,
+    if value in sign:
+        # If the input value isn't number,
+        # input again for 'Arithmetic' function.
         _sign = value
         print("Entered :", num_log, _sign)
-        value_2 = input("Enter : ") # input again for 'Arithmetic' function.
-        if value_2 not in sign: # And checking...
-            num_2 = int(value_2) # Changing...
-            new_num = Arithmetic(num_1, _sign, num_2) # Running...
-            print("Entered :", new_num) # Printing!
+        value_2 = input("Enter : ")
+        if value_2 not in sign:
+            num_2 = int(value_2)
+            new_num = Arithmetic(num_1, _sign, num_2)
+            print("Entered :", new_num)
             num_1 = new_num
             num_log = new_num
         else:
-            _sign = value_2 # If the third input value isn't number,
+            # If the third input value isn't number,
+            # initializing sign value and input again.
+            _sign = value_2
             print("Entered :", num_log, _sign)
-            value_2 = input("Enter : ") # initializing sign value and input again.
-            num_2 = int(value_2) # Changing...
-            new_num = Arithmetic(num_1, _sign, num_2) # Running...
-            print("Entered :", new_num) # Printing!
+            value_2 = input("Enter : ")
+            num_2 = int(value_2)
+            new_num = Arithmetic(num_1, _sign, num_2)
+            print("Entered :", new_num)
             num_1 = new_num
             num_log = new_num
 
-    else: # If the first input value is number, it is performed.
+    else:
+        # If the first input value is number,
+        # it is performed.
         print("Entered :", value)
         num_log = value
         num_1 = int(value)
-
