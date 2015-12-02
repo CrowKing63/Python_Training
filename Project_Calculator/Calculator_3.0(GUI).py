154275d0f6a48faf8ca42efd223078303e25e6da
uitaek91 <choiuitaek@gmail.com>
2015-12-02 14:37:54 +0900

    Cal 3.0

---
+++ Project_Calculator/Calculator_3.0(GUI).py
@@ -0,0 +1,47 @@
+import tkinter
+CAL_BUTTON = ['CE', 'C', 'BS', 'Log',
+              '7',  '8', '9', '/',
+              '4',  '5', '6', '*',
+              '1',  '2', '3', '-',
+              '0',  '.', '=', '+']
+CAL_RESULT = 0
+NUM_CHECK = ('0','1','2','3','4','5','6','7','8','9','.')
+SIGN_CHECK = ('+','-','*','/')
+BUTTON_SIZE = 5
+GAP = 10
+ROW = 1
+COL = 0
+NUM = []
+PUSH_RESULT = "".join(NUM)
+
+master = tkinter.Tk()
+master.title('Calculator')
+result = tkinter.Label(
+    master,
+    text = PUSH_RESULT,
+    width = BUTTON_SIZE * 5,
+    heigh = BUTTON_SIZE // 2 + 1,
+    borderwidth = 1,
+    relief = 'sunken',
+    background = 'lightgreen',
+    padx = GAP,
+    anchor = 'e'
+)
+result.grid(row = 0, column = 0, columnspan = 4)
+
+for i in CAL_BUTTON:
+    tkinter.Button(
+        master,
+        text = i,
+        width = BUTTON_SIZE,
+        height = BUTTON_SIZE // 2,
+        # command = Push
+    ).grid(
+        row = ROW,
+        column = COL
+    )
+    COL += 1
+    if COL > 3:
+        COL = 0
+        ROW += 1
+tkinter.mainloop()
